db33f1ca05ac5dab406a457feed7495cf618c8e3
DAVIDE SAVARRO <20023866@studenti.uniupo.it>
2023-06-02 09:15:15 +0200

    Modified exampleLang in EELang

--- src/test/java/org/mal_lang/eelang/test/ExampleLangTest.java
+++ src/test/java/org/mal_lang/eelang/test/ExampleLangTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.mal_lang.examplelang.test;
+package org.mal_lang.eelang.test;
 
 import core.Asset;
 import core.AttackStep;
